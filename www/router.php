9bb522ab9363a8b5d7e6d54183228829550f1c26
Harold Paulson <haroldp@internal.org>
2013-10-05 14:12:57 -0700

    get api started

--- www/router.php
+++ www/router.php
@@ -1,1 +1,19 @@
-coming soon
+<?
+
+$request_path = preg_replace(
+	'/^([^\?]+).*$/', 
+	'\\1', 
+	$_SERVER['REQUEST_URI']
+);
+
+switch ($request_path) {
+	case '/about':
+		$page = 'about.php';
+		break;
+	default:
+		$page = '404.php';
+}
+
+require_once($page);
+
+?>
